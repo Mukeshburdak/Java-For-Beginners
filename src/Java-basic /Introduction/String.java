a3bd052d0e8369d98c653d3ad7d1890035ff2696
Mukesh Burdak <mburdak626@gmail.com>
2026-01-22 23:03:55 +0530

    Add files via upload

---
+++ src/Java-basic /Introduction/String.java
@@ -0,0 +1,16 @@
+class string {
+    public static void main(String[] args) {
+        String s = "My name is Mukesh.";
+        System.out.println(s);
+        System.out.println(s.charAt(1));
+        System.out.println(s.charAt(9));
+
+        String s1 = """
+                college name: Central University of Jammu.
+                Degree: Btech.
+                year:2nd,4th semester.
+                specialization:computer science and engineering.
+                """;
+        System.out .println(s1);
+    }
+}
