960b7205471153a78cb02c8147c9b0845b191a6f
Mukesh Burdak <mburdak626@gmail.com>
2026-01-22 23:12:20 +0530

    Add files via upload

---
+++ src/Operators & Control Statements/Arithmetic_operators.java
@@ -0,0 +1,23 @@
+class Main{
+    public static void main(String[] args){
+        int a = 30;
+        int b = 20;
+        //Addition
+        System.out.println(a+b);
+
+        //Subtract
+        System.out.println(a-b);
+
+        //Multiplication
+        System.out.println(a*b);
+
+        //Division
+        System.out.println(a/b);
+
+        //Float division
+        System.out.println(a/(float)b);
+
+        //Modulo operator - Remainder
+        System.out.println(a%b);
+    }
+}
